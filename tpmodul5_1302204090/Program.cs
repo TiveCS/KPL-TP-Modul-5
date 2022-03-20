ce953aa1f2d64dcb5a238db96a30148afa23a5ab
TiveCS <creativecode.id@gmail.com>
2022-03-21 01:40:10 +0700

    menambahkan penjelasan

--- tpmodul5_1302204090/Program.cs
+++ tpmodul5_1302204090/Program.cs
@@ -11,18 +11,22 @@ namespace tpmodul5_1302204090
 		static void Main(string[] args)
 		{
 
+			// membuat instance class
 			SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Ahmad Fathan Hanif");
 			video.IncreasePlayCount(15);
 			video.PrintVideoDetails();
 
+			// test set title dengan null
 			video.SetTitle(null);
 			video.IncreasePlayCount(11235);
 			video.PrintVideoDetails();
 
+			// test menambahkan play count melebihi 10.000.000
 			video.IncreasePlayCount(120000000);
 			video.PrintVideoDetails();
 
-			for (int i = 0; i < 1000; i++)
+			// test play count jika kapasitas variabel melebihi batas akan melakukan try-catch
+			for (int i = 0; i < 218; i++)
 			{
 				video.IncreasePlayCount(10000000);
 			}
